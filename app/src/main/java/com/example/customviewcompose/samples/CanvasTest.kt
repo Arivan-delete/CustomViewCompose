6d07a24409d7e0b87cd9656d70c0dddaaad3afbf
Artem <artem.mamedov5375@gmail.com>
2024-02-15 14:26:29 +0200

    Brush and Path

--- app/src/main/java/com/example/customviewcompose/samples/CanvasTest.kt
+++ app/src/main/java/com/example/customviewcompose/samples/CanvasTest.kt
@@ -6,97 +6,53 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun CanvasTest() {
     Canvas(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black)
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(Color.Cyan, Color.Magenta),
+                    start = Offset(100.dp.toPx(), 0.dp.toPx()),
+                    end = Offset(100.dp.toPx(), 100.dp.toPx()),
+                    tileMode = TileMode.Mirror
+                )
+            )
     ) {
-        //H
-        drawLine(
+        drawPath(
+            path = Path().apply {
+                moveTo(center.x, 100.dp.toPx())
+                lineTo(center.x + 25.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x + 75.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x + 45.dp.toPx(), 195.dp.toPx())
+                lineTo(center.x + 60.dp.toPx(), 250.dp.toPx())
+                lineTo(center.x, 220.dp.toPx())
+                lineTo(center.x - 60.dp.toPx(), 250.dp.toPx())
+                lineTo(center.x - 45.dp.toPx(), 195.dp.toPx())
+                lineTo(center.x - 75.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x - 25.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x, 100.dp.toPx())
+            },
             color = Color.White,
-            start = Offset(50.dp.toPx(), 25.dp.toPx()),
-            end = Offset(50.dp.toPx(), 75.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.White,
-            start = Offset(50.dp.toPx(), 50.dp.toPx()),
-            end = Offset(75.dp.toPx(), 50.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.White,
-            start = Offset(75. dp.toPx(), 25.dp.toPx()),
-            end = Offset(75.dp.toPx(), 75.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-
-        //E
-        drawLine(
-            color = Color.White,
-            start = Offset(100.dp.toPx(), 25.dp.toPx()),
-            end = Offset(100.dp.toPx(), 75.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.White,
-            start = Offset(100.dp.toPx(), 25.dp.toPx()),
-            end = Offset(125.dp.toPx(), 25.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.White,
-            start = Offset(100.dp.toPx(), 50.dp.toPx()),
-            end = Offset(125.dp.toPx(), 50.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.White,
-            start = Offset(100.dp.toPx(), 75.dp.toPx()),
-            end = Offset(125.dp.toPx(), 75.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-
-        //L
-        drawLine(
-            color = Color.White,
-            start = Offset(150.dp.toPx(), 25.dp.toPx()),
-            end = Offset(150.dp.toPx(), 75.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.White,
-            start = Offset(150.dp.toPx(), 75.dp.toPx()),
-            end = Offset(175.dp.toPx(), 75.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-
-        //L
-        drawLine(
-            color = Color.White,
-            start = Offset(200.dp.toPx(), 25.dp.toPx()),
-            end = Offset(200.dp.toPx(), 75.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.White,
-            start = Offset(200.dp.toPx(), 75.dp.toPx()),
-            end = Offset(225.dp.toPx(), 75.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-
-        //O
-        drawCircle(
-            color = Color.White,
-            radius = 25.dp.toPx(),
-            center = Offset(275.dp.toPx(), 50.dp.toPx()),
-            style = Stroke(width = 1.dp.toPx())
+            style = Fill
         )
     }
+}
+
+@Composable
+fun Dp.toPx() = with(LocalDensity.current) {
+    this@toPx.toPx()
 }
